07ee80ffbe016d4560fb8726e30ade7f665021cc
Serhii <sergiiriabokon@gmail.com>
2021-05-25 15:51:37 +0300

    added CURL clojure wrapper with sample

--- src/clojure_image_load/core.clj
+++ src/clojure_image_load/core.clj
@@ -1,7 +1,34 @@
 (ns clojure-image-load.core
-  (:gen-class))
+  (:require [clj-curl.easy :as curl-easy]
+            [clj-curl.opts :as opts])
+  (:import [clj_curl.Handlers FileHandler]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def +universe-image-url+ 
+  "https://images.universe.com/ace26761-7e6f-4271-a6ba-73c27f3169f8/-/quality/lightest/-/format/jpeg/")
+
+(def +universe-local-filepath+ "./universe.jpg")
+
+(defn url->file! 
+  [url filepath]
+  ;FileHandler needs to be closed after you finished to use it.
+  ;creating it with 'with-open' does that automatically for you.
+  (with-open [filehldr (FileHandler. filepath)]
+    (let [curl (curl-easy/init)]
+      (do
+        (println "simple http")
+        (curl-easy/setopt curl opts/url url)
+        (curl-easy/setopt curl opts/writefunction filehldr)
+        (curl-easy/perform curl)
+        (println "speed download: " (curl-easy/getinfo-double curl opts/speed-download))
+        (curl-easy/cleanup curl)))))
+;;
+;; Sample for using CURL library wrapper for clojure.
+;; Suitable to substitute error cases with
+;; clojure.javaio./input-stream 
+;;
+(defn -main []
+  (url->file! +universe-image-url+ 
+              +universe-local-filepath+))
+
+;; Commented code below produces error 451
+#_(clojure.java.io/input-stream +universe-image-url+)
